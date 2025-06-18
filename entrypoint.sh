4d4a5cbf7a710739e7bf26a063900c054d081e0b
gracelsy <43689473+gracelsy@users.noreply.github.com>
2025-06-18 11:50:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ -n "$GIT_USER" ]; then
-  git config --global user.name "$GIT_USER"
-fi
-if [ -n "$GIT_EMAIL" ]; then
-  git config --global user.email "$GIT_EMAIL"
-fi
-exec sh
+
+# 配置 Git 用户信息（如果环境变量已设置）
+[ -n "$GIT_USER" ] && git config --global user.name "$GIT_USER"
+[ -n "$GIT_EMAIL" ] && git config --global user.email "$GIT_EMAIL"
+
+# 执行传递给容器的命令，默认执行 CMD 指定的命令
+exec "$@"
